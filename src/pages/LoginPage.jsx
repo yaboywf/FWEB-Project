0b4d45739c79992519346b64bd168d9925c742cd
Dylan <dylanyeowf@gmail.com>
2025-10-31 18:50:22 +0800

    updated auth flow

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,8 @@ import { useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../general/UserProvider";
 import styles from "../styles/login.module.scss";
+import axios from "redaxios";
+import REQ from "../general/Request";
 
 const LoginPage = () => {
     const navigate = useNavigate();
