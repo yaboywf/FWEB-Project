9f6f783c343c0897f9a4a84844c5069b4205adf6
Dylan <dylanyeowf@gmail.com>
2025-11-04 12:19:55 +0800

    added rate limiting

--- controller/account.js
+++ controller/account.js
@@ -1,29 +1,29 @@
 import express from "express";
-import { verify, checkRequiredKeys } from "../middleware.js";
+import { verify, checkRequiredKeys, writeLimiter } from "../middleware.js";
 import User from "../database/users.js";
 import Achievement from "../database/achievements.js";
 import Attained from "../database/attained.js";
 
 const router = express.Router();
 
-router.get("/information", verify, checkRequiredKeys('query', ["id"]), async (req, res) => {
+router.get("/information", verify, writeLimiter, checkRequiredKeys('query', ["id"]), async (req, res) => {
     const account = await User.findOne({ student_id: req.query.id });
     if (!account) return res.status(404).json({ message: "Account not found" });
 
     return res.json(account);
 })
 
-router.get("/all-achievements", verify, async (req, res) => {
+router.get("/all-achievements", verify, writeLimiter, async (req, res) => {
     const achievements = await Achievement.find({ student_id: req.user.student_id }).sort({ difficulty: 1 });
     return res.json(achievements);
 })
 
-router.get("/attained-achievements", verify, async (req, res) => {
+router.get("/attained-achievements", verify, writeLimiter, async (req, res) => {
     const achievements = await Attained.find({ student_id: req.user.student_id });
     return res.json(achievements);
 })
 
-router.post("/rating", verify, checkRequiredKeys('body', ["student_id", "rating"]), async (req, res) => {
+router.post("/rating", verify, writeLimiter, checkRequiredKeys('body', ["student_id", "rating"]), async (req, res) => {
     if (req.user.student_id === req.body.student_id) return res.status(400).json({ message: "Cannot rate yourself" });
     if (req.body.rating < 1 || req.body.rating > 5) return res.status(400).json({ message: "Rating must be between 1 and 5" });
 
@@ -35,7 +35,7 @@ router.post("/rating", verify, checkRequiredKeys('body', ["student_id", "rating"
     return res.json({ message: "Rating submitted successfully" });
 })
 
-router.put("/update", verify, checkRequiredKeys('body', ["diploma", "year_of_study"]), async (req, res) => {
+router.put("/update", verify, writeLimiter, checkRequiredKeys('body', ["diploma", "year_of_study"]), async (req, res) => {
     try {
         const { diploma, year_of_study } = req.body;
         const updatedAccount = await User.findOneAndUpdate(
